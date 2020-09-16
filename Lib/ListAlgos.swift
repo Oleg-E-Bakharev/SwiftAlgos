4541f5555882c553fcacce2b9aad2728cb893bd6
Oleg Bakharev <oleg_bakharev@mail.ru>
2020-09-16 11:57:49 +0300

    Слияние отсортированных списков

--- Lib/ListAlgos.swift
+++ Lib/ListAlgos.swift
@@ -26,8 +26,39 @@ public extension List {
     }
     
     /// On-time O1-memory. Destructive merge sorted lists inplace
-    static func mergeSorted(lhs: inout List, rhs: inout List) -> List {
-        let list = List()
-        return list
+    static func mergeSorted(lhs: inout List, rhs: inout List, compare: (Value, Value)->Bool) -> List {
+        var list = List()
+        
+        func mergeStep(_ left: inout Node?, _ right: inout Node?) -> Node? {
+            guard left != nil && right != nil else { return nil }
+            guard let someLeft = left else {
+                defer { right = right?.next }
+                return right
+            }
+            guard let someRight = right else {
+                defer { left = left?.next }
+                return left
+            }
+            
+            if compare(someLeft.value, someRight.value) {
+                defer { left = left?.next }
+                return left
+            }
+            defer { right = right?.next }
+            return right
+        }
+
+        var right = rhs.head
+        var left = lhs.head
+        
+        list.setHead(mergeStep(&left, &right))
+        list.setTail(list.tail)
+        repeat {
+            guard let node = mergeStep(&left, &right) else {
+                return list
+            }
+            list.tail?.next = node
+            list.setTail(node)
+        } while(true)
     }
 }
